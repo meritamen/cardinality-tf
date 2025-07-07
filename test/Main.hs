80ecd2536af70d6f86a36437273360f1587b8d9c
Mrjt Jmn <meritamen@sdf.org>
2025-07-07 18:23:51 +0800

    Add naive unit test

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,18 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Main (main) where
 
+import Cardinality
+import GHC.Generics
+import Test.Hspec
+
+data Color = Red | Green | Blue
+  deriving (Generic, Show, Eq, TypeCardinality)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = hspec $ do
+  describe "cardinality of custom data types" $ do
+    it "Color has cardinality 3" $ do
+      cardinality @Color `shouldBe` Left 3
